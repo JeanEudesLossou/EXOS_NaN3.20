cc8ac6f98614f29da9ea9b6ba3fb48ac60bd945d
JeanEudesLossou <adamahjean@gmail.com>
2020-03-12 09:54:35 +0100

    la reduction de ton code

---
+++ nouveau_nan.py
@@ -0,0 +1,9 @@
+#Tests avec l'age
+age = int(input("donner votre age : "))
+if age <= 20:
+	print("Vous êtes mineur !!!")
+elif age > 20 and age <= 45:
+	print("Vous êtes autoriser à integrer !")
+else:
+	print("Vous êtes vieux disons !!! b")
+
