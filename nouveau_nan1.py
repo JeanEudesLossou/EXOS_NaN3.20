653ae363ce7eca89333f3aeae6efdd5573611b59
JeanEudesLossou <adamahjean@gmail.com>
2020-03-26 20:09:18 +0100

    modif du nmm projet

---
+++ nouveau_nan1.py
@@ -0,0 +1,12 @@
+#Tests avec l'age
+age = int(input("donner votre age : "))
+if age <= 20:
+	print("Vous êtes mineur !!!")
+elif age > 20 and age <= 45:
+	print("Vous êtes autoriser à integrer !")
+else:
+	print("Vous êtes vieux disons !!! b")
+
+print("Invitation accepter !!!")
+ 
+
